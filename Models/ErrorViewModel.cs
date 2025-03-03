8ecc337212bb7d56c6e21f92dfb67660469c19d1
Ivan Plastinin <plastininbiz@yandex.ru>
2025-03-03 19:28:12 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NameCraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
